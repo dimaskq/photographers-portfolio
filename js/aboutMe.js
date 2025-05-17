06612c5569728b6acaf732038918bbd67e390a9a
dimaskq <dimassskq@gmail.com>
2025-05-17 15:12:22 +0300

    seo title and h1

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -7,7 +7,7 @@ const aboutHTML = `<div class="aboutMe">
                     </div>
                     <div class="aboutMe__info"  data-aos="fade-left">
                         <h2 class="aboutMe__containerTitle" data-i18n="aboutme-title">About Me</h2>
-                        <h1 class="aboutMe__title" data-i18n="aboutme-subtitle">My name is Oksana Manuilikova, <br>I’m a <span>Content Creator, Videographer</span></h1>
+                        <h3 class="aboutMe__title" data-i18n="aboutme-subtitle">My name is Oksana Manuilikova, <br>I’m a <span>Content Creator, Videographer</span></h3>
                         <div class="aboutMe__line"></div>
                         <p class="aboutMe__text" data-i18n="aboutme-text">Every project for me is more than just a shoot. I immerse myself in your story, atmosphere, and meaning to create a video that not only looks beautiful but also works.
                             I film for businesses, brands, experts, and creators who want to be seen.
